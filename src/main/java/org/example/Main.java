943ec6a08ab8a3cf1b47a0e2d4ecb594c80bcec0
barbarahellmann <babshellmann@web.de>
2024-03-08 16:49:10 +0100

    Group challenge done

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,29 @@
 package org.example;
 
+//Write a test for a method that calculates the sum of two integers. --> Done
+//Write a test for a method that checks if a given number is even. --> Done
+//Write a test for a method that calculates the product of two integers. --> Done
+//Write a test for a method that converts a given string to uppercase.
+//Write a test for a method that checks if a given number is positive.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
     }
+    public static int calculateSum (int a,int b){
+        return (a+b);
+    }
+
+    public static boolean isEven (int c){
+        return (c%2==0);
+    }
+    public static int multiply (int a,int b){
+        return (a*b);
+    }
+    public static String toUpperCase (String text){
+        return text.toUpperCase();
+    }
+
+    public static boolean isPositive (int a){
+        return (a>0);
+    }
+
 }
--- src/test/java/org/example/MainTest.java
+++ src/test/java/org/example/MainTest.java
@@ -1,4 +1,74 @@
+package org.example;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class MainTest {
-  
+    @Test
+    void calculateSum_shouldReturn20_whenCalledWith5and15(){
+    //GIVEN
+        int a=5;
+        int b=15;
+        int expected = 20;
+    // WHEN
+        int actual = Main.calculateSum(a,b);
+    //THEN
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void isEven_shouldReturnTrue_whenCalledWith8(){
+        //GIVEN
+        int c=8;
+        //WHEN
+        boolean actual = Main.isEven(c);
+        //THEN
+        Assertions.assertTrue(actual);
+    }
+
+    @Test
+    void multiply_shouldReturn56_whenCalledWith7and8(){
+        //GIVEN
+        int a=7;
+        int b=8;
+        int expected = 56;
+        //WHEN
+        int actual = Main.multiply(a,b);
+        //THEN
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void toUpperCase_shouldReturnABCDEF_whenCalledWithabcdef(){
+        //GIVEN
+        String text = "abcdef";
+        String expected = "ABCDEF";
+        //WHEN
+        String actual = Main.toUpperCase(text);
+        //THEN
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void isPositive_shouldReturnTrue_whenCalledWith5() {
+        //GIVEN
+        int a = 5;
+        //WHEN
+        boolean actual = Main.isPositive(a);
+        //THEN
+        Assertions.assertTrue(actual);
+    }
+    @Test
+    void isPositive_shouldReturnFalse_whenCalledWith0() {
+        //GIVEN
+        int a = 0;
+        //WHEN
+        boolean actual = Main.isPositive(a);
+        //THEN
+        Assertions.assertFalse(actual);
+    }
+
+
 }
